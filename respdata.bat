b850c5203408f13b69e246eade24589b83903f97
cesar <cesar.utleon@gmail.com>
2020-08-24 15:02:35 -0500

    4 ARCHIVOS

---
+++ respdata.bat
@@ -0,0 +1,7 @@
+start /b
+XCOPY "C:\MySQL\MySQL Server 5.1\data\*.*" "\\CAJA\respdata\" /D/S/H/V/C/F/K/Y
+exit
+
+
+
+
---
+++ respdatac.bat
@@ -0,0 +1,7 @@
+start /b
+XCOPY "C:\MySQL\MySQL Server 5.1\*.*" "C:\MySonda\resp\" /D/S/H/V/C/F/K/Y
+exit
+
+
+
+
