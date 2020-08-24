3053b4b70e0690971baf3f19b4a592b67fd8fe97
cesar <cesar.utleon@gmail.com>
2020-08-24 11:38:31 -0500

    archivos de pinki

---
+++ odbcusuario.bat
@@ -0,0 +1,3 @@
+cd C:\Windows\SysWOW64\
+ODBCCONF.exe /a { CONFIGDSN "MySQL ODBC 3.51 Driver" "DSN=basedatos|SERVER=GOVJOY01PCS01|Database=basedatos|user=prestafacil|password=pfacil|port =3307|Trusted_Connection=Si"}
+ODBCCONF.exe /a { CONFIGDSN "MySQL ODBC 3.51 Driver" "DSN=basereportes|SERVER=GOVJOY01PCS01|Database=basedatos|user=prestafacil|password=pfacil|port=3307|Trusted_Connection=Si"}
